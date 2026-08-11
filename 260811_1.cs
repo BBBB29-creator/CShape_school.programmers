3c518bd693a98ee215d3086ccbd99b83e5a0f8bc
BBBB29-creator <coree09@naver.com>
2026-08-11 23:50:40 +0900

    [Lv.1] 크기가 작은 부분 문자열
    문제 풀이

---
+++ 260811_1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Solution
+{
+    public int solution(string t, string p)
+    {
+        int answer = 0;
+        int len_p = p.Length;
+        int len_t = t.Length;
+
+        long num_p = long.Parse(p);
+
+        for (int i = 0; i <= len_t - len_p; i++)
+        {
+            string subStr = t.Substring(i, len_p);
+
+            if (long.Parse(subStr) <= num_p)
+                answer++;
+        }
+        return answer;
+    }
+}
