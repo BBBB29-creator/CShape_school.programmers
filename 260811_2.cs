9875ced981ff553782109a0697591710d97d0d9a
BBBB29-creator <coree09@naver.com>
2026-08-12 00:02:10 +0900

    [Lv.1] 가장 가까운 같은 글자
    프로그래머스 문제 답안

---
+++ 260811_2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Solution
+{
+    public int[] solution(string s)
+    {
+        int[] answer = new int[s.Length];
+        int[] lastPos = new int[26];
+
+        for (int i = 0; i < 26; i++)
+        {
+            lastPos[i] = -1;
+        }
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            int alphabetIndex = s[i] - 'a';
+
+            if (lastPos[alphabetIndex] == -1)
+            {
+                answer[i] = -1;
+            }
+            else
+            {
+                answer[i] = i - lastPos[alphabetIndex];
+            }
+            lastPos[alphabetIndex] = i;
+        }
+        return answer;
+    }
+}
