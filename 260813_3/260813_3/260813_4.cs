b47332d9b3bb32b78fb01e5fffba9e5374ca9118
BBBB29-creator <coree09@naver.com>
2026-08-13 23:45:40 +0900

    [Lv.0] 대소문자 바꿔서 출력하기
    처음엔 그런 기능을 가진 함수가 있는지 찾아봤다. 0 레벨 인데도 식 작성이 쉽지가 않다...

---
+++ 260813_3/260813_3/260813_4.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        string s = Console.ReadLine();
+
+        foreach (char c in s)
+        {
+            if (char.IsUpper(c))
+                Console.Write(char.ToLower(c));
+            else
+                Console.Write(char.ToUpper(c));
+        }
+    }
+}
