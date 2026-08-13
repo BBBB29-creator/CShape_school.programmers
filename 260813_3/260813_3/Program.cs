0b7f1efe0ca7eab58be2a5a70d29c03fe8920cba
BBBB29-creator <coree09@naver.com>
2026-08-13 23:37:19 +0900

    [Lv.0] 문자열 반복해서 출력하기
    기초 문제 파트인데 기초가 아닌 듯한 느낌...

---
+++ 260813_3/260813_3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        string[] input = Console.ReadLine().Split(' ');
+
+        string s1 = input[0];
+
+        int n = int.Parse(input[1]);
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(s1);
+        }
+    }
+}
